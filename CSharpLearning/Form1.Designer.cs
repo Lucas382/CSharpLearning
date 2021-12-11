36998b59aece59e773df620e6d7e81c503453c35
Lucas Alves <lucasalves382@hotmail.com>
2021-12-11 15:17:27 -0300

    Add project files.

---
+++ CSharpLearning/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace CSharpLearning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlNav = new System.Windows.Forms.Panel();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnContactus = new System.Windows.Forms.Button();
+            this.btnCalendar = new System.Windows.Forms.Button();
+            this.btnAnalytics = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label_timer = new System.Windows.Forms.Label();
+            this.label_user_login_time = new System.Windows.Forms.Label();
+            this.label_user_name = new System.Windows.Forms.Label();
+            this.userAvatar = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.userAvatar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.pnlNav);
+            this.panel1.Controls.Add(this.btnSettings);
+            this.panel1.Controls.Add(this.btnContactus);
+            this.panel1.Controls.Add(this.btnCalendar);
+            this.panel1.Controls.Add(this.btnAnalytics);
+            this.panel1.Controls.Add(this.btnDashboard);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlNav
+            // 
+            this.pnlNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.pnlNav.Location = new System.Drawing.Point(0, 193);
+            this.pnlNav.Name = "pnlNav";
+            this.pnlNav.Size = new System.Drawing.Size(3, 100);
+            this.pnlNav.TabIndex = 4;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnSettings.Image = global::CSharpLearning.Properties.Resources.settings___1365_1;
+            this.btnSettings.Location = new System.Drawing.Point(0, 535);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(186, 42);
+            this.btnSettings.TabIndex = 3;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            this.btnSettings.Leave += new System.EventHandler(this.btnSettings_Leave);
+            // 
+            // btnContactus
+            // 
+            this.btnContactus.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnContactus.FlatAppearance.BorderSize = 0;
+            this.btnContactus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContactus.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContactus.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnContactus.Image = global::CSharpLearning.Properties.Resources.profile_image___1362_;
+            this.btnContactus.Location = new System.Drawing.Point(0, 270);
+            this.btnContactus.Name = "btnContactus";
+            this.btnContactus.Size = new System.Drawing.Size(186, 42);
+            this.btnContactus.TabIndex = 2;
+            this.btnContactus.Text = "Contact Us";
+            this.btnContactus.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnContactus.UseVisualStyleBackColor = true;
+            this.btnContactus.Click += new System.EventHandler(this.btnContactus_Click);
+            this.btnContactus.Leave += new System.EventHandler(this.btnContactus_Leave);
+            // 
+            // btnCalendar
+            // 
+            this.btnCalendar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCalendar.FlatAppearance.BorderSize = 0;
+            this.btnCalendar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalendar.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalendar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnCalendar.Image = global::CSharpLearning.Properties.Resources.calendar___1195_;
+            this.btnCalendar.Location = new System.Drawing.Point(0, 228);
+            this.btnCalendar.Name = "btnCalendar";
+            this.btnCalendar.Size = new System.Drawing.Size(186, 42);
+            this.btnCalendar.TabIndex = 2;
+            this.btnCalendar.Text = "Calendar";
+            this.btnCalendar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnCalendar.UseVisualStyleBackColor = true;
+            this.btnCalendar.Click += new System.EventHandler(this.btnCalendar_Click);
+            this.btnCalendar.Leave += new System.EventHandler(this.btnCalendar_Leave);
+            // 
+            // btnAnalytics
+            // 
+            this.btnAnalytics.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAnalytics.FlatAppearance.BorderSize = 0;
+            this.btnAnalytics.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAnalytics.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnalytics.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnAnalytics.Image = global::CSharpLearning.Properties.Resources.stats___1370_;
+            this.btnAnalytics.Location = new System.Drawing.Point(0, 186);
+            this.btnAnalytics.Name = "btnAnalytics";
+            this.btnAnalytics.Size = new System.Drawing.Size(186, 42);
+            this.btnAnalytics.TabIndex = 2;
+            this.btnAnalytics.Text = "Analytics";
+            this.btnAnalytics.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAnalytics.UseVisualStyleBackColor = true;
+            this.btnAnalytics.Click += new System.EventHandler(this.btnAnalytics_Click);
+            this.btnAnalytics.Leave += new System.EventHandler(this.btnAnalytics_Leave);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnDashboard.Image = global::CSharpLearning.Properties.Resources.home___1391_;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 144);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(186, 42);
+            this.btnDashboard.TabIndex = 2;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
+            this.btnDashboard.Leave += new System.EventHandler(this.btnDashboard_Leave);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label_timer);
+            this.panel2.Controls.Add(this.label_user_login_time);
+            this.panel2.Controls.Add(this.label_user_name);
+            this.panel2.Controls.Add(this.userAvatar);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 144);
+            this.panel2.TabIndex = 1;
+            // 
+            // label_timer
+            // 
+            this.label_timer.AutoSize = true;
+            this.label_timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_timer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(178)))));
+            this.label_timer.Location = new System.Drawing.Point(57, 130);
+            this.label_timer.Name = "label_timer";
+            this.label_timer.Size = new System.Drawing.Size(80, 13);
+            this.label_timer.TabIndex = 3;
+            this.label_timer.Text = "Logged time:";
+            // 
+            // label_user_login_time
+            // 
+            this.label_user_login_time.AutoSize = true;
+            this.label_user_login_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_user_login_time.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(178)))));
+            this.label_user_login_time.Location = new System.Drawing.Point(134, 131);
+            this.label_user_login_time.Name = "label_user_login_time";
+            this.label_user_login_time.Size = new System.Drawing.Size(49, 12);
+            this.label_user_login_time.TabIndex = 2;
+            this.label_user_login_time.Text = "00:00:00";
+            // 
+            // label_user_name
+            // 
+            this.label_user_name.AutoSize = true;
+            this.label_user_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_user_name.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(149)))));
+            this.label_user_name.Location = new System.Drawing.Point(48, 88);
+            this.label_user_name.Name = "label_user_name";
+            this.label_user_name.Size = new System.Drawing.Size(85, 16);
+            this.label_user_name.TabIndex = 1;
+            this.label_user_name.Text = "User Name";
+            // 
+            // userAvatar
+            // 
+            this.userAvatar.Image = global::CSharpLearning.Properties.Resources.user_icon_png_pnglogocom_133466;
+            this.userAvatar.InitialImage = global::CSharpLearning.Properties.Resources.profile___1335_;
+            this.userAvatar.Location = new System.Drawing.Point(60, 22);
+            this.userAvatar.Name = "userAvatar";
+            this.userAvatar.Size = new System.Drawing.Size(63, 63);
+            this.userAvatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.userAvatar.TabIndex = 0;
+            this.userAvatar.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.userAvatar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label_user_login_time;
+        private System.Windows.Forms.Label label_user_name;
+        private System.Windows.Forms.PictureBox userAvatar;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Label label_timer;
+        private System.Windows.Forms.Button btnContactus;
+        private System.Windows.Forms.Button btnCalendar;
+        private System.Windows.Forms.Button btnAnalytics;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Panel pnlNav;
+    }
+}
+
